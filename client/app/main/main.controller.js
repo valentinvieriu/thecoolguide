0788b3810bd95ef8de783420920f3d1bbb4c18ca
Valentin Vieriu <valentin.vieriu@gmail.com>
2014-10-27 07:34:51 +0100

    improved map; added clustering

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,25 +1,51 @@
 'use strict';
 
 angular.module('thecoolguideApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, $q, socket) {
     $scope.awesomeThings = [];
     $scope.munichCenter = {
           lat: 48.137447477273,
           lng: 11.578306599531 ,
           zoom: 13
       };
+      var naturalEarth = L.tileLayer('http://{s}.tiles.mapbox.com/v3/mapbox.natural-earth-2/{z}/{x}/{y}.png');
+      //xyz,mapbox,geoJSON,utfGrid,cartodbTiles,cartodbUTFGrid,cartodbInteractive,wms,wmts,wfs,group,featureGroup,google,china,ags,dynamic,markercluster,bing,heatmap,yandex,imageOverlay,custom,cartodb
+
     $scope.layers = {
             baselayers: {
-                googleHybrid: {
-                    name: 'Google Hybrid',
-                    layerType: 'HYBRID',
-                    type: 'google'
-                },
-                googleRoadmap: {
-                    name: 'Google Streets',
-                    layerType: 'ROADMAP',
-                    type: 'google'
-                }
+              mapbox: {
+                  name: 'Mapbox',
+                  type: 'xyz',
+                  url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+                  layerParams: {
+                      apikey: 'pk.eyJ1IjoidmFsZW50aW52aWVyaXUiLCJhIjoibzRqVk02TSJ9.o80Jz3QZDUAExWNqe8xKXQ',
+                      mapid: 'valentinvieriu.k2e5m77g'
+                  },
+                  layerOptions: {
+                      subdomains: ['a', 'b', 'c'],
+                      continuousWorld: true
+                  }
+              }, 
+              cycle: {
+                  name: 'OpenCycleMap',
+                  type: 'xyz',
+                  url: 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
+                  layerOptions: {
+                      subdomains: ['a', 'b', 'c'],
+                      attribution: '&copy; <a href="http://www.opencyclemap.org/copyright">OpenCycleMap</a> contributors - &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                      continuousWorld: true
+                  }
+              },
+              osm: {
+                  name: 'OpenStreetMap',
+                  type: 'xyz',
+                  url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  layerOptions: {
+                      subdomains: ['a', 'b', 'c'],
+                      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                      continuousWorld: true
+                  }
+              }
             }
         };
     $http.get('/api/things').success(function(awesomeThings) {
@@ -28,7 +54,8 @@ angular.module('thecoolguideApp')
     }); 
 
     $http.get('api/feeds/facebook')
-    .then(function(fdData) {
+    
+    .then(function(fdData) { //we get the latest feeds and hide the junk ones
       var responseData = fdData.data.data;
       var cleanResult = [];
       var locationIds = [];
@@ -47,38 +74,67 @@ angular.module('thecoolguideApp')
         };
         
       });
+
+      $scope.fbData = cleanResult;
+
       return {
         fbData:cleanResult,
         locationIds:locationIds
       }
-    }).then(function(result){
-      // console.log(result.locationIds.length)
-      var locations = [];
-      angular.forEach(result.locationIds.slice(0,50),function(fbPage){
-        locations.push({'method':'GET','relative_url':fbPage+'/?fields=name,location,category,category_list'});
-      });
-      // console.log(JSON.stringify(locations));
+    })
 
-      $scope.fbData = result.fbData;
-      // .join(',')
-      return $http.post('api/feeds/facebookBatch/',{'batch':JSON.stringify(locations)});
+    .then(function(result){ //We process the locations for the remaining ones.We need to batch process 50 requests for FB
+      var locations      = [];
+      var countLocations = result.locationIds.length;
+      var qRequests      = [];
+
+      for (var i =  0; i <= Math.floor(countLocations/50); i++) {
+        locations = [];
+        angular.forEach(result.locationIds.slice(i*50,(i+1)*50) ,function(fbPage){
+          locations.push({'method':'GET','relative_url':fbPage+'/?fields=name,location,category,category_list,cover'});
+        });
+
+        qRequests.push($http.post('api/feeds/facebookBatch/',{'batch':JSON.stringify(locations)}));
+      };
+
+
+      return $q.all(qRequests);
 
     })
+
+
+    .then(function(results){ //we merge the responses into one
+      var mergedResults = [];
+
+      angular.forEach(results,function(resultsItem){
+        mergedResults = mergedResults.concat(resultsItem.data);
+      });
+
+      return mergedResults;
+
+    })
+
+
     .then(function(result){
       var mapList = {};
-      // console.log(result.data);
-      angular.forEach(result.data,function(item){
+
+      angular.forEach(result,function(item){
         var page = JSON.parse(item.body);
-        // var currentItem = {};
+
         if (page.location && page.location.latitude && page.location.longitude) {
+          var allCategories = [];
+          angular.forEach(page.category_list,function(categodyList){
+            allCategories.push(categodyList.name);
+          });
+
           mapList['marker_'+page.id] = {
-              lat       :page.location.latitude,
-              lng       :page.location.longitude,
-              message   :page.name,
+              group     : 'munich',
+              lat       : page.location.latitude,
+              lng       : page.location.longitude,
+              message   : ( ( (page.cover && page.cover.source) ? '<img src="'+page.cover.source+'" width="130" />': '' ) + '<h4>'+page.name+'</h4><p>'+allCategories.join(', ')+'</p>' ),
               draggable : false
             };
-          // mapList.push(currentItem);
-          // console.log(page.name,page.location.latitude,page.location.longitude)
+
         };
       });
       $scope.markers = mapList;
